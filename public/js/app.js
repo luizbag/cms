f6ec9979869f464a3f87fb7e67165f38f7b4c9e1
Luiz Gavioli <luizbag@gmail.com>
2017-09-01 09:57:21 -0300

    Adding layout for list of entries

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,16 @@
 var app = angular.module('cms', ['ngAnimate']);
 
-app.controller('Comments', ['$scope', function($scope) {
-    $scope.items = [];
-    $scope.addItem = function() {
-        $scope.items.push($scope.item);
-        $scope.item = {};
-    };
-}]);
+app.controller("PostController", ['$window', function($window) {
+    var ctrl = this;
+
+    ctrl.posts = [
+        {title: 'Title 1',
+         thumb: 'https://img.solarmovie.sc/2016/05/28/cover/ec68847b234cdf9f28757f3e17cf8c83-the-last-man-on-the-moon-1464460083.jpg'},
+        {title: 'Title 2', thumb: 'https://img.solarmovie.sc/2016/05/28/cover/ec68847b234cdf9f28757f3e17cf8c83-the-last-man-on-the-moon-1464460083.jpg'},
+        {title: 'Title 3', thumb: 'https://img.solarmovie.sc/2016/05/28/cover/ec68847b234cdf9f28757f3e17cf8c83-the-last-man-on-the-moon-1464460083.jpg'},
+        {title: 'Title 4', thumb: 'https://img.solarmovie.sc/2016/05/28/cover/ec68847b234cdf9f28757f3e17cf8c83-the-last-man-on-the-moon-1464460083.jpg'},
+        {title: 'Title 5', thumb: 'https://img.solarmovie.sc/2016/05/28/cover/ec68847b234cdf9f28757f3e17cf8c83-the-last-man-on-the-moon-1464460083.jpg'},
+        {title: 'Title 6', thumb: 'https://img.solarmovie.sc/2016/05/28/cover/ec68847b234cdf9f28757f3e17cf8c83-the-last-man-on-the-moon-1464460083.jpg'},
+        {title: 'Title 7', thumb: 'https://img.solarmovie.sc/2016/05/28/cover/ec68847b234cdf9f28757f3e17cf8c83-the-last-man-on-the-moon-1464460083.jpg'},
+    ]
+}])
